9d65feb1840ec88dc276e81b7b30b51aafa169d3
accenture.putra <putra.nugraha@accenture.com>
2021-12-24 23:46:57 +0700

    refactor move user list fragment to fragment dir

--- app/src/main/java/com/nsystem/features/usersearch/presentation/fragment/UserListFragment.kt
+++ app/src/main/java/com/nsystem/features/usersearch/presentation/fragment/UserListFragment.kt
@@ -1,4 +1,4 @@
-package com.nsystem.features.usersearch.presentation
+package com.nsystem.features.usersearch.presentation.fragment
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
