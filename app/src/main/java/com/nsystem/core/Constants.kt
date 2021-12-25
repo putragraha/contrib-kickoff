84b7cb51397b5f3765e14029dd08ca6488655468
accenture.putra <putra.nugraha@accenture.com>
2021-12-25 17:33:43 +0700

    create retrofit instance

--- app/src/main/java/com/nsystem/core/Constants.kt
+++ app/src/main/java/com/nsystem/core/Constants.kt
@@ -3,4 +3,6 @@ package com.nsystem.core
 object Constants {
 
     const val DB_NAME = "GitHubUser"
+
+    const val API_BASE_URL = "https://api.github.com/"
 }
--- app/src/main/java/com/nsystem/core/di/CoreModule.kt
+++ app/src/main/java/com/nsystem/core/di/CoreModule.kt
@@ -9,6 +9,9 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 import javax.inject.Singleton
 
 @Module
@@ -20,6 +23,23 @@ class CoreModule {
     fun provideRoomDatabase(@ApplicationContext context: Context): ApplicationDatabase {
         return Room
             .databaseBuilder(context, ApplicationDatabase::class.java, Constants.DB_NAME)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(Constants.API_BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(MoshiConverterFactory.create())
             .build()
     }
 }
