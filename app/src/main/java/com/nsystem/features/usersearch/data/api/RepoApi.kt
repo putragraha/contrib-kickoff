a069731d7ac6144395339fa4bc6524e8e7cb6420
putragraha <putra.graha.7@gmail.com>
2022-01-04 11:51:45 +0700

    update repo api

--- app/src/main/java/com/nsystem/features/usersearch/data/api/RepoApi.kt
+++ app/src/main/java/com/nsystem/features/usersearch/data/api/RepoApi.kt
@@ -1,17 +1,18 @@
 package com.nsystem.features.usersearch.data.api
 
-import com.nsystem.features.usersearch.data.model.RepoOld
+import com.nsystem.features.usersearch.data.model.UserSearch
 import retrofit2.Response
 import retrofit2.http.GET
-import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface RepoApi {
 
-    @GET("users/{username}/repos")
-    suspend fun getUserRepo(
-        @Path("username") username: String,
+    @GET("search/repositories")
+    suspend fun searchUser(
+        @Query("q") query: String,
         @Query("page") page: Int,
-        @Query("per_page") perPage: Int
-    ): Response<List<RepoOld>>
+        @Query("per_page") perPage: Int,
+        @Query("sort") sort: String,
+        @Query("order") order: String,
+    ): Response<UserSearch>
 }
