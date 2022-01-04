8d93e559f1d03edc995483b051f466e29e81915a
putragraha <putra.graha.7@gmail.com>
2022-01-04 11:50:31 +0700

    update repo repository contract

--- app/src/main/java/com/nsystem/features/usersearch/domain/repository/RepoRepository.kt
+++ app/src/main/java/com/nsystem/features/usersearch/domain/repository/RepoRepository.kt
@@ -4,7 +4,7 @@ import androidx.paging.PagingData
 import com.nsystem.features.usersearch.domain.model.Repo
 import kotlinx.coroutines.flow.Flow
 
-interface RepoRepository {
+interface RepoRepository<T : Repo> {
 
-    suspend fun searchRepo(query: String): Flow<PagingData<Repo>>
+    suspend fun searchRepo(query: String): Flow<PagingData<T>>
 }
