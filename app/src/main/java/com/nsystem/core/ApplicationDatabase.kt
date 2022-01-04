36ce2d18aadd6eb3aa4b96ce0226a3c8ad08d424
putragraha <putra.graha.7@gmail.com>
2022-01-04 09:37:30 +0700

    create repo dao

--- app/src/main/java/com/nsystem/core/ApplicationDatabase.kt
+++ app/src/main/java/com/nsystem/core/ApplicationDatabase.kt
@@ -2,6 +2,7 @@ package com.nsystem.core
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.nsystem.features.usersearch.data.dao.RepoDao
 import com.nsystem.features.usersearch.data.dao.UserDao
 import com.nsystem.features.usersearch.data.dao.UserRemoteKeysDao
 import com.nsystem.features.usersearch.data.model.User
@@ -17,6 +18,7 @@ import com.nsystem.features.usersearch.data.model.UserRemoteKeys
 )
 abstract class ApplicationDatabase: RoomDatabase() {
 
+    abstract fun repoDao(): RepoDao
     abstract fun userDao(): UserDao
     abstract fun userRemoteKeysDaoDao(): UserRemoteKeysDao
 }
