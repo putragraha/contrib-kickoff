579cc5d33a29ab1935675a37fc93fd20c9257831
putragraha <putra.graha.7@gmail.com>
2022-01-05 10:48:21 +0700

    update repo api by adding search repo function

--- app/src/main/java/com/nsystem/features/usersearch/data/api/RepoApi.kt
+++ app/src/main/java/com/nsystem/features/usersearch/data/api/RepoApi.kt
@@ -1,17 +1,24 @@
 package com.nsystem.features.usersearch.data.api
 
-import com.nsystem.features.usersearch.data.model.UserSearch
-import retrofit2.Response
+import com.nsystem.features.usersearch.data.model.RepoSearchResponse
 import retrofit2.http.GET
 import retrofit2.http.Query
+
+const val IN_QUALIFIER = "in:name,description"
+
+const val DEFAULT_TAG = "help-wanted-issues"
+
+const val INITIAL_PAGE_INDEX = 1
+
+const val DEFAULT_PAGE_SIZE = 10
 
 interface RepoApi {
 
     @GET("search/repositories")
-    suspend fun searchUser(
+    suspend fun searchRepo(
         @Query("q") query: String,
         @Query("page") page: Int,
         @Query("per_page") perPage: Int,
         @Query("sort") sort: String
-    ): Response<UserSearch>
+    ): RepoSearchResponse
 }
