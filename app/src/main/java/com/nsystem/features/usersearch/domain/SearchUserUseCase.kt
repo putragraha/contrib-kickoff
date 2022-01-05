dc09de3bd35d08e18eca88ea0fc7e855c071b313
putragraha <putra.graha.7@gmail.com>
2022-01-05 14:05:18 +0700

    remove user search view model

--- app/src/main/java/com/nsystem/features/usersearch/domain/SearchUserUseCase.kt
+++ app/src/main/java/com/nsystem/features/usersearch/domain/SearchUserUseCase.kt
@@ -4,6 +4,7 @@ import androidx.paging.ExperimentalPagingApi
 import com.nsystem.features.usersearch.data.repository.UserSearchRepository
 import javax.inject.Inject
 
+// TODO: 05/01/22 Remove use case after the viewmodel removed
 class SearchUserUseCase @Inject constructor(private val repository: UserSearchRepository) {
 
     @ExperimentalPagingApi
