2bd696ce1fcf98bd2202c524db0191c94cb51158
putragraha <putra.graha.7@gmail.com>
2022-01-05 10:49:23 +0700

    update repo data repository to use mediator as data source

---
+++ app/src/main/java/com/nsystem/features/usersearch/data/repository/RepoDataRepository.kt
@@ -0,0 +1,35 @@
+package com.nsystem.features.usersearch.data.repository
+
+import androidx.paging.ExperimentalPagingApi
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import com.nsystem.core.ApplicationDatabase
+import com.nsystem.features.usersearch.data.api.DEFAULT_PAGE_SIZE
+import com.nsystem.features.usersearch.data.api.RepoApi
+import com.nsystem.features.usersearch.data.model.RepoEntity
+import com.nsystem.features.usersearch.domain.repository.RepoRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class RepoDataRepository @Inject constructor(
+    private val applicationDatabase: ApplicationDatabase,
+    private val repoApi: RepoApi
+): RepoRepository<RepoEntity> {
+
+    override suspend fun searchRepo(query: String): Flow<PagingData<RepoEntity>> {
+        val dbQuery = "%${query.replace(' ', '%')}%"
+        val pagingSourceFactory = { applicationDatabase.repoDao().searchRepo(dbQuery) }
+
+        @OptIn(ExperimentalPagingApi::class)
+        return Pager(
+            config = PagingConfig(pageSize = DEFAULT_PAGE_SIZE),
+            remoteMediator = RepoRemoteMediator(
+                query = query,
+                repoApi = repoApi,
+                applicationDatabase = applicationDatabase
+            ),
+            pagingSourceFactory = pagingSourceFactory
+        ).flow
+    }
+}
