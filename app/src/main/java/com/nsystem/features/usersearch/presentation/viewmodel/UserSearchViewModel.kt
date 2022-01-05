ed42ca15e9214db75af02b7649faadb2eb6ab803
putragraha <putra.graha.7@gmail.com>
2022-01-05 14:04:07 +0700

    remove user search fragment

--- app/src/main/java/com/nsystem/features/usersearch/presentation/viewmodel/UserSearchViewModel.kt
+++ app/src/main/java/com/nsystem/features/usersearch/presentation/viewmodel/UserSearchViewModel.kt
@@ -12,6 +12,7 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.collect
 import javax.inject.Inject
 
+// TODO: 05/01/22 Remove after Fragment removed
 @HiltViewModel
 class UserSearchViewModel @Inject constructor(
     private val searchUserUseCase: SearchUserUseCase
