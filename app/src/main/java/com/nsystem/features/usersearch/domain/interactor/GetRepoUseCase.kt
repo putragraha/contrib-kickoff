fa7ef765eb070e8fb5cc32cd0b12eba298efa396
putragraha <putra.graha.7@gmail.com>
2022-01-05 10:51:54 +0700

    update get repo interactor repository args

--- app/src/main/java/com/nsystem/features/usersearch/domain/interactor/GetRepoUseCase.kt
+++ app/src/main/java/com/nsystem/features/usersearch/domain/interactor/GetRepoUseCase.kt
@@ -1,9 +1,10 @@
 package com.nsystem.features.usersearch.domain.interactor
 
+import com.nsystem.features.usersearch.domain.model.Repo
 import com.nsystem.features.usersearch.domain.repository.RepoRepository
 import javax.inject.Inject
 
-class GetRepoUseCase @Inject constructor(private val repoRepository: RepoRepository) {
+class GetRepoUseCase @Inject constructor(private val repoRepository: RepoRepository<Repo>) {
 
-    fun invoke(query: String) = repoRepository.searchRepo(query)
+    suspend fun invoke(query: String) = repoRepository.searchRepo(query)
 }
