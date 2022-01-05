1dc3f138bbfdf28965f25ba8bae8f6092e938086
putragraha <putra.graha.7@gmail.com>
2022-01-05 13:53:58 +0700

    add divider on repo list recyclerview

--- app/src/main/java/com/nsystem/features/usersearch/presentation/fragment/SearchRepoFragment.kt
+++ app/src/main/java/com/nsystem/features/usersearch/presentation/fragment/SearchRepoFragment.kt
@@ -4,7 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.RecyclerView
+import com.nsystem.R
 import com.nsystem.databinding.FragmentSearchRepoBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -22,8 +26,33 @@ class SearchRepoFragment: Fragment() {
         return binding?.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupView()
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         binding = null
+    }
+
+    private fun setupView() {
+        binding?.rvSearchResult?.run {
+            addItemDecoration(getRepoListDivider())
+        }
+    }
+
+    private fun getRepoListDivider(): DividerItemDecoration {
+        val dividerDrawable = ResourcesCompat.getDrawable(
+            resources,
+            R.drawable.layer_user_list_divider,
+            null
+        )
+
+        return DividerItemDecoration(context, RecyclerView.VERTICAL).apply {
+            dividerDrawable?.let {
+                setDrawable(dividerDrawable)
+            }
+        }
     }
 }
