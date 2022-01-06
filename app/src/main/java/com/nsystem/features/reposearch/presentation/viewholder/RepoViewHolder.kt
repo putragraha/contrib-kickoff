be90386d7db27a967fb0826fe097929c66ca9e45
putragraha <putra.graha.7@gmail.com>
2022-01-06 10:24:51 +0700

    create handle if owner name is null

--- app/src/main/java/com/nsystem/features/reposearch/presentation/viewholder/RepoViewHolder.kt
+++ app/src/main/java/com/nsystem/features/reposearch/presentation/viewholder/RepoViewHolder.kt
@@ -2,6 +2,7 @@ package com.nsystem.features.reposearch.presentation.viewholder
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import com.nsystem.R
 import com.nsystem.databinding.ItemRepoBinding
@@ -10,10 +11,11 @@ import com.nsystem.features.reposearch.domain.model.Repo
 class RepoViewHolder(private val binding: ItemRepoBinding): RecyclerView.ViewHolder(binding.root) {
 
     fun bind(repo: Repo) {
+        val ownerName = repo.ownerName ?: ""
+        if (ownerName.isBlank()) binding.tvOwnerName.isVisible = false
+
         binding.tvRepoName.text = repo.fullName
-        repo.ownerName?.let { ownerName ->
-            binding.tvOwnerName.text = getText(R.string.label_owner_name, ownerName)
-        }
+        binding.tvOwnerName.text = getText(R.string.label_owner_name, ownerName)
         binding.actvStar.text = getText(R.plurals.label_star, repo.starCount)
         binding.actvFork.text = getText(R.plurals.label_fork, repo.forkCount)
         binding.actvIssue.text = getText(R.plurals.label_issue, repo.openIssueCount)
